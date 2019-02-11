7226f37a002cc476d58160be09286d9bd7a4c690
Yunus Alkan <alkannyunus@gmail.com>
2019-02-11 11:57:46 +0300

    Refactor

--- iTunesAPILegacy/iTunesAPILegacy/View/Media/Detail/MediaDetailViewController.swift
+++ iTunesAPILegacy/iTunesAPILegacy/View/Media/Detail/MediaDetailViewController.swift
@@ -27,8 +27,6 @@ class MediaDetailViewController: BaseViewController, ViewModelBased {
 
         trackButton.setImage(trackButton.imageView?.image?.withRenderingMode(.alwaysTemplate), for: .normal)
         trackButton.titleLabel?.numberOfLines = 0
-
-//        setupVideoPlayer()
         bindings()
     }
 
@@ -68,25 +66,6 @@ class MediaDetailViewController: BaseViewController, ViewModelBased {
         navigationItem.rightBarButtonItem = deleteBtn
     }
 
-    func setupVideoPlayer() {
-//        playerController = AVPlayerViewController()
-//        playerController.view.backgroundColor = .clear
-//
-//        self.addChild(playerController)
-//        previewView.addSubview(playerController.view)
-//        playerController.view.frame = previewView.frame
-//
-//        // layout
-//        previewView.translatesAutoresizingMaskIntoConstraints = false
-//        playerController.view.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            playerController.view.leadingAnchor.constraint(equalTo: previewView.leadingAnchor),
-//            playerController.view.trailingAnchor.constraint(equalTo: previewView.trailingAnchor),
-//            playerController.view.topAnchor.constraint(equalTo: previewView.topAnchor),
-//            playerController.view.bottomAnchor.constraint(equalTo: previewView.bottomAnchor)
-//            ])
-    }
-
     @objc func deleteTapped() {
         ConfirmationPopup.create(title: "POPUP_ARE_YOU_SURE_WANT_TO_DELETE".localized,
                                  completion: { [weak self] state in
@@ -102,28 +81,12 @@ class MediaDetailViewController: BaseViewController, ViewModelBased {
     }
 
     @IBAction func trackTapped(_ sender: Any) {
-        // test purposes for playing track
+        // TEST: test purposes for playing track
 
         if let prevUrl = viewModel.element.value?.previewUrl, let videoUrl = URL(string: prevUrl) {
             let vc = MediaPlayerViewController.initFromNib()
             vc.videoUrl = videoUrl
             self.present(vc, animated: true, completion: nil)
-//            let playerItem = AVPlayerItem(url: URL(string: prevUrl)!)
-//            let videoPlayer = AVPlayer(playerItem: playerItem)
-//            videoPlayer.actionAtItemEnd = .none
-//
-//            let playerController = AVPlayerViewController()
-//            playerController.view.frame = view.frame
-//
-//            let vc = UIViewController()
-//            vc.view.frame = view.frame
-//            vc.view.backgroundColor = .black
-//
-//            vc.addChild(playerController)
-//            vc.view.addSubview(playerController.view)
-//
-//            playerController.player = videoPlayer
-//            playerController.player?.play()
         }
     }
 
